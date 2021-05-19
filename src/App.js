c9c46641e361355cfd8d31cf12eca8f09d0c89e5
Christian Guevara <christianjguevara@hotmail.com>
2021-05-19 17:31:51 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from "react";
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
+  /* cargar citas de localstorage como state inicial */
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  /* useState retorna 2 funciones */
+  /* El state actual = this.state; */
+  /* Funcion que actualiza el state this.setState() */
+
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  /* Agregar las nuevas citar al state */
+
+  const crearCita = cita => {
+    /* Tomar una copia del state y agregar el nuevo cliente */
+
+    const nuevaCitas = [...citas, cita];
+
+    /* almacenamos en el state */
+    guardarCitas(nuevaCitas);
+    console.log(nuevaCitas);
+  };
+
+  /* Elimina las citas del State */
+
+  const eliminarCita = index => {
+    const nuevasCitas = [...citas];
+    nuevasCitas.splice(index, 1);
+    guardarCitas(nuevasCitas);
+  };
+
+  useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas]);
+
+  /* cargar condicionalmente un titulo */
+  /* object.keys(array); lo que hace es devolver las osiciones de cada elemento de un objeto o array */
+  const titulo =
+    Object.keys(citas).length === 0
+      ? "No hay Citas"
+      : "Admnistrar las Citas aqui";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map((cita, index) => (
+              <Cita
+                key={index}
+                index={index}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Cita.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Cita({ index, cita, eliminarCita }) {
+  return (
+    <div className="cita">
+      <p>
+        Mascota: <span>{cita.mascota}</span>
+      </p>
+      <p>
+        Dueño: <span>{cita.propietario}</span>
+      </p>
+      <p>
+        Fecha: <span>{cita.fecha}</span>
+      </p>
+      <p>
+        Hora: <span>{cita.hora}</span>
+      </p>
+      <p>
+        Sintomas: <span>{cita.sintomas}</span>
+      </p>
+      <button
+        onClick={index => eliminarCita(index)}
+        type="button"
+        className="button eliminar u-full-width"
+      >
+        Eliminar X
+      </button>
+    </div>
+  );
+}
+
+export default Cita;
